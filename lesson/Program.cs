8325382e338913b11b2afa6ddd0801621ffe108b
Dvoretskiy_Sergey <umbrella18@mail.ru>
2023-05-29 15:17:11 +0300

    сделал задания 5 семинара

--- lesson/Program.cs
+++ lesson/Program.cs
@@ -95,60 +95,60 @@ X Y могу меняться время от времени.
 
 //Заполнение массива
 
-// void FillArr(int[] array)
-// {
-//     Random rnd = new Random(); // создаем удобный формат рандомных чисел
-//     int a = 0; // создаем переменную для работы цикла
-//     while(a < array.Length) // запускаем цикл и проверяем, пока переменная А меньше длинны атрибута array. 
-//     //Цикл повторяется
-//     {
-//         array[a] = rnd.Next(-9,10); // каждый раз при цикле будет генерироваться случайно число из диапазона
-//         a++; //  и прибавляться значение в переменную А, пока она не станет равна длинне массива
-//     }
-// }
+void FillArr(int[] array)
+{
+    Random rnd = new Random(); // создаем удобный формат рандомных чисел
+    int a = 0; // создаем переменную для работы цикла
+    while(a < array.Length) // запускаем цикл и проверяем, пока переменная А меньше длинны атрибута array. 
+    //Цикл повторяется
+    {
+        array[a] = rnd.Next(-9,10); // каждый раз при цикле будет генерироваться случайно число из диапазона
+        a++; //  и прибавляться значение в переменную А, пока она не станет равна длинне массива
+    }
+}
 
-// //Вывод массива
-// void PrintArr(int[] Narray)
-// {
-//     int count = Narray.Length;
-//     int pos = 0;
-//     while(pos < count)
-//     {
-//         Write($"{Narray[pos]},");
-//         pos++;
-//     }
-// }
+//Вывод массива
+void PrintArr(int[] Narray)
+{
+    int count = Narray.Length;
+    int pos = 0;
+    while(pos < count)
+    {
+        Write($"{Narray[pos]},");
+        pos++;
+    }
+}
 
-// // проверяем на положительные и отрицательные числа
-// void summ (int[] array)
-// {
-//     int count = 0; // счетчик
-//     int SumMin = 0; // тут будут сумма отрицательных чисел
-//     int SumPlus = 0; //  тут сумма положительныех чисел
-//     while(count < array.Length)
-//     {
-//         if(array[count] < 0)
-//         {
-//             SumMin += array[count];
-//             count++;
-//         }
-//         else
-//         {
-//             SumPlus += array[count];
-//             count++;
-//         }
-//     }
-//     WriteLine($"Сумма отрц чисел {SumMin}");
-//     WriteLine($"Сумма полж чисел {SumPlus}");
-// }
+// проверяем на положительные и отрицательные числа
+void summ (int[] array)
+{
+    int count = 0; // счетчик
+    int SumMin = 0; // тут будут сумма отрицательных чисел
+    int SumPlus = 0; //  тут сумма положительныех чисел
+    while(count < array.Length)
+    {
+        if(array[count] < 0)
+        {
+            SumMin += array[count];
+            count++;
+        }
+        else
+        {
+            SumPlus += array[count];
+            count++;
+        }
+    }
+    WriteLine($"Сумма отрц чисел {SumMin}");
+    WriteLine($"Сумма полж чисел {SumPlus}");
+}
 
-// // вызвать
+// вызвать
 
-// int[]arr = new int[12];
-// FillArr(arr); // заполнение массива
-// PrintArr(arr); // печать массива
-// Write("\n"); // перенос строки
-// summ(arr); // сумма отр или пол. чисел
+int[]arr = new int[12];
+FillArr(arr); // заполнение массива
+PrintArr(arr); // печать массива
+Write("\n"); // перенос строки
+summ(arr); // сумма отр или пол. чисел
 
 
 // Задача 32: Напишите программу замена элементов массива: положительные элементы замените 
@@ -156,43 +156,43 @@ X Y могу меняться время от времени.
 // [-4, -8, 8, 2] -> [4, 8, -8, -2]
 
 
-// создаем массив
-// void CreateArray(int[] array)
-// {
-//     Random rnd = new Random();
-//     int count = 0;
-//     while(count < array.Length)
-//     {
-//         Write($"{array[count] = rnd.Next(-9,9)},");
-//         count++;
-//     }
-// }
+//создаем массив
+void CreateArray(int[] array)
+{
+    Random rnd = new Random();
+    int count = 0;
+    while(count < array.Length)
+    {
+        Write($"{array[count] = rnd.Next(-9,9)},");
+        count++;
+    }
+}
 
-// void reversArray(int[] revers)
-// {
-//     int count = 0;
-//     int rev = 0;
-//     while(count < revers.Length)
-//     {
-//         if (revers[count] < 0)
-//         {
-//             rev =+ Math.Abs(revers[count]);
-//             count++;
-//         }
-//         else
-//         {
-//             rev =+ -revers[count];
-//             count++;
-//         }
-//         Write($"{rev},");
-//     } 
-// }
+void reversArray(int[] revers)
+{
+    int count = 0;
+    int rev = 0;
+    while(count < revers.Length)
+    {
+        if (revers[count] < 0)
+        {
+            rev =+ Math.Abs(revers[count]);
+            count++;
+        }
+        else
+        {
+            rev =+ -revers[count];
+            count++;
+        }
+        Write($"{rev},");
+    } 
+}
 
 
-// int[] arr = new int[3];
-// CreateArray(arr);
-// Write("\n");
-// reversArray(arr);
+int[] arr = new int[3];
+CreateArray(arr);
+Write("\n");
+reversArray(arr);
 
 
 
@@ -207,43 +207,43 @@ X Y могу меняться время от времени.
 
 
 
-// создаем массив
-// void CreateArray(int[] array)
-// {
-//     Random rnd = new Random();
-//     int count = 0;
-//     while(count < array.Length)
-//     {
-//         Write($"{array[count] = rnd.Next(1,9)},");
-//         count++;
-//     }
-// }
+//создаем массив
+void CreateArray(int[] array)
+{
+    Random rnd = new Random();
+    int count = 0;
+    while(count < array.Length)
+    {
+        Write($"{array[count] = rnd.Next(1,9)},");
+        count++;
+    }
+}
 
-// // ищем цифры
-// void findarray(int[] find)
-// {
-//     int a = 0;
-//     int con = 3;
-//     while(a < find.Length)
-//     {
-//         if(find[a] == con)
-//         {
-//             WriteLine($"цифра {con} есть в массиве");
-//             break;
-//         }
-//         else
-//         {
-//             //WriteLine("данной цифры нет");
-//             a++;
-//         }
-//     }
-// }
+// ищем цифры
+void findarray(int[] find)
+{
+    int a = 0;
+    int con = 3;
+    while(a < find.Length)
+    {
+        if(find[a] == con)
+        {
+            WriteLine($"цифра {con} есть в массиве");
+            break;
+        }
+        else
+        {
+            //WriteLine("данной цифры нет");
+            a++;
+        }
+    }
+}
 
 
-// int[] arr = new int[5];
-// CreateArray(arr);
-// Write("\n");
-// findarray(arr);
+int[] arr = new int[5];
+CreateArray(arr);
+Write("\n");
+findarray(arr);
 
 
 // Задача 35: Задайте одномерный массив из 123 случайных чисел. Найдите количество элементов массива, 
@@ -253,37 +253,37 @@ X Y могу меняться время от времени.
 // [1, 2, 3, 6, 2] -> 0
 // [10, 11, 12, 13, 14] -> 5
 
-// void CreateArray(int[] array)
-// {
-//     Random rnd = new Random();
-//     int count = 0;
-//     while(count < array.Length)
-//     {
-//         Write($"{array[count] = rnd.Next(1,123)},");
-//         count++;
-//     }
-// }
+void CreateArray(int[] array)
+{
+    Random rnd = new Random();
+    int count = 0;
+    while(count < array.Length)
+    {
+        Write($"{array[count] = rnd.Next(1,123)},");
+        count++;
+    }
+}
 
-// void finearray(int[] find)
-// {
-//     int a = 0;
-//     int b = 0;
-//     while(a < find.Length)
-//     {
-//         if(find[a] > 10 && find[a] < 100)
-//         {
-//             b++;
-//         }
-//         a++;
-//     }
-//     WriteLine($"Значений, который лежат в отрезке от 10 до 99: {b}");
-// }
+void finearray(int[] find)
+{
+    int a = 0;
+    int b = 0;
+    while(a < find.Length)
+    {
+        if(find[a] > 10 && find[a] < 100)
+        {
+            b++;
+        }
+        a++;
+    }
+    WriteLine($"Значений, который лежат в отрезке от 10 до 99: {b}");
+}
 
 
-// int[] arr = new int[5];
-// CreateArray(arr);
-// Write("\n");
-// finearray(arr);
+int[] arr = new int[5];
+CreateArray(arr);
+Write("\n");
+finearray(arr);
 
 
 
@@ -295,38 +295,38 @@ X Y могу меняться время от времени.
 // [6 7 3 6] -> 36 21
 
 
-// void CreateArray(int[] array)
-// {
-//     Random rnd = new Random();
-//     int count = 0;
-//     while(count < array.Length)
-//     {
-//         Write($"{array[count] = rnd.Next(1,5)} ");
-//         count++;
-//     }
-// }
+void CreateArray(int[] array)
+{
+    Random rnd = new Random();
+    int count = 0;
+    while(count < array.Length)
+    {
+        Write($"{array[count] = rnd.Next(1,5)} ");
+        count++;
+    }
+}
 
-// // считаем данные
-// void findnumber(int[] numb)
-// {
-//     int count = 0;
-//     int num = numb.Length;
-//     int aa = 0;
-//     int bb = numb.Length;
-//     while(count < numb.Length)
-//     {
-//         num = numb[aa] * numb[bb-1];
-//         Write($"{num} ");
-//         aa++;
-//         bb--;
-//         count++;
-//     }
-// }
+// считаем данные
+void findnumber(int[] numb)
+{
+    int count = 0;
+    int num = numb.Length;
+    int aa = 0;
+    int bb = numb.Length;
+    while(count < numb.Length)
+    {
+        num = numb[aa] * numb[bb-1];
+        Write($"{num} ");
+        aa++;
+        bb--;
+        count++;
+    }
+}
 
 
-// int[] arr = new int[5];
-// CreateArray(arr);
-// Write("\n");
-// findnumber(arr);
+int[] arr = new int[5];
+CreateArray(arr);
+Write("\n");
+findnumber(arr);
 
 
